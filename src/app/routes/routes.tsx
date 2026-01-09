767447014dfa52c376f420f605b855c5375a87c4
j4rviscmd <j4rviscmd@gmail.com>
2026-01-10 03:45:27 +0900

    revert: disable route lazy loading

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,18 +1,15 @@
 import type { RouteObject } from 'react-router'
 
+import { HomePage } from '../../pages/home'
+import { SettingsPage } from '../../pages/settings'
+
 export const routes: RouteObject[] = [
   {
     path: '/',
-    lazy: async () => {
-      const { HomePage } = await import('../../pages/home/index')
-      return { Component: HomePage }
-    },
+    element: <HomePage />,
   },
   {
     path: '/settings',
-    lazy: async () => {
-      const { SettingsPage } = await import('../../pages/settings/index')
-      return { Component: SettingsPage }
-    },
+    element: <SettingsPage />,
   },
 ]
