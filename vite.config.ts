45fdb27998fbf61bf381f0d6183929e55bb6a925
j4rviscmd <j4rviscmd@gmail.com>
2026-01-10 03:48:10 +0900

    chore: add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@/app': fileURLToPath(new URL('./src/app', import.meta.url)),
+      '@/pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@/features': fileURLToPath(new URL('./src/features', import.meta.url)),
+      '@/shared': fileURLToPath(new URL('./src/shared', import.meta.url)),
     },
   },
   server: {
